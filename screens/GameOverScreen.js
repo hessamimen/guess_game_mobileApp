14d6d62c9c057741eb6815ec7d0c78b93bf634f7
Hessam <hessam.imen@dijgtal.com>
2023-06-02 17:13:43 +1000

    feat: add the check for game over

---
+++ screens/GameOverScreen.js
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native";
+
+function GameOverScreen() {
+  return <Text>Gaem is over</Text>;
+}
+
+export default GameOverScreen;
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,7 @@
 import { Text, View, StyleSheet, Alert } from "react-native";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PrimaryButton from "../components/ui/PrimaryButton";
 
 function generateRandomBetween(min, max, exclude) {
@@ -17,13 +17,15 @@ function generateRandomBetween(min, max, exclude) {
 let minBoundary = 1;
 let maxBoundary = 100;
 
-function GameScreen({ userNumber }) {
-  const initialGuess = generateRandomBetween(
-    minBoundary,
-    maxBoundary,
-    userNumber
-  );
+function GameScreen({ userNumber, onGameOver }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver();
+    }
+  }, [currentGuess, userNumber, onGameOver]);
 
   function nextGuessHandler(direction) {
     // direction => 'lower' or 'higher
