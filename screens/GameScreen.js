7bcb3f3522275687c2ae260ac0316beaaaeb6f17
Hessam <hessam.imen@dijgtal.com>
2023-06-03 21:34:04 +1000

    style: working with icons - add button Icons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,7 +1,8 @@
+import { useEffect, useState } from "react";
 import { View, StyleSheet, Alert } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import Title from "../components/ui/Title";
 import NumberContainer from "../components/game/NumberContainer";
-import { useEffect, useState } from "react";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
@@ -65,12 +66,12 @@ function GameScreen({ userNumber, onGameOver }) {
         <View style={styles.bottonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
-              -
+              <Ionicons name="md-remove" size={24} color="white" />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, "higher")}>
-              +
+              <Ionicons name="md-add" size={24} color="white" />
             </PrimaryButton>
           </View>
         </View>
