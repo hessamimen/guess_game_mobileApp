47ab1e8798933348dff24652e42642226560684a
Hessam <hessam.imen@dijgtal.com>
2023-06-10 13:42:52 +1000

    feat: write Platform specific code with the Platform API for Title compoennt

---
+++ components/ui/Title.android.js
@@ -0,0 +1,25 @@
+import { Text, StyleSheet } from "react-native";
+
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+    // fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    // borderWidth: Platform.OS === "android" ? 2 : 0,
+    // borderWidth: Platform.select({ ios: 0, android: 2 }),
+    borderWidth: 2,
+    borderColor: "white",
+    padding: 12,
+    borderRadius: 6,
+    width: 300,
+    maxWidth: "80%",
+  },
+});
